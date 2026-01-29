f1840c81dcdf73bc429fc9cab1b97e23a88181c7
Brandon Sebastian <123838599+nooburger81@users.noreply.github.com>
2026-01-29 17:55:58 -0500

    added all sorts of goodies, and first ability - dash

---
+++ Source/GAS_Starter_Template/GameplayAbilitySystem/AttributeSets/BasicAttributeSet.h
@@ -0,0 +1,66 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AttributeSet.h"
+#include "BasicAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_STARTER_TEMPLATE_API UBasicAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+
+public:
+
+	UBasicAttributeSet();
+
+	//Health Attributes
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing = OnRep_Health)
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS_BASIC(UBasicAttributeSet, Health);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing = OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS_BASIC(UBasicAttributeSet, MaxHealth);
+
+	//Stamina Attributes
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing = OnRep_Stamina)
+	FGameplayAttributeData Stamina;
+	ATTRIBUTE_ACCESSORS_BASIC(UBasicAttributeSet, Stamina);
+
+	UPROPERTY(BlueprintReadOnly, Category = "Attributes", ReplicatedUsing = OnRep_MaxStamina)
+	FGameplayAttributeData MaxStamina;
+	ATTRIBUTE_ACCESSORS_BASIC(UBasicAttributeSet, MaxStamina);
+
+public:
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldValue) const
+	{
+		GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, Health, OldValue);
+	}
+
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldValue) const
+	{
+		GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, MaxHealth, OldValue);
+	}
+
+	UFUNCTION()
+	void OnRep_Stamina(const FGameplayAttributeData& OldValue) const
+	{
+		GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, Stamina, OldValue);
+	}
+
+	UFUNCTION()
+	void OnRep_MaxStamina(const FGameplayAttributeData& OldValue) const
+	{
+		GAMEPLAYATTRIBUTE_REPNOTIFY(UBasicAttributeSet, MaxStamina, OldValue);
+	}
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+};
